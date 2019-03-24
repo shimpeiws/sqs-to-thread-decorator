cc5f7ac19a94d235cebcaf9f1de4ffdef9795444
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-03-24 22:36:26 +0900

    Rename

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-setup()
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='queue_for_thread',
+    version='0.1.0',
+    description='Queue For Thread',
+    long_description=readme,
+    author='Shimpei Takamatsu',
+    author_email='shimpeiws@gmail.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license='MIT',
+)
