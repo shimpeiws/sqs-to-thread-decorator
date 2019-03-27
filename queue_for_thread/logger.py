2c76bb89e2ecb7434afb561e77cdf277df0a6f94
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-03-28 00:52:48 +0900

    Fix

--- queue_for_thread/logger.py
+++ queue_for_thread/logger.py
@@ -4,9 +4,10 @@ from logging import getLogger, StreamHandler, Formatter, INFO
 class Logger:
     def __init__(self, **options):
         logger = getLogger(__name__)
-        logger.setLevel(options.get('log_level', INFO))
+        log_level = options.get('log_level', INFO)
+        logger.setLevel(log_level)
         stream_handler = StreamHandler()
-        stream_handler.setLevel(self.log_level)
+        stream_handler.setLevel(log_level)
         handler_format = Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         stream_handler.setFormatter(handler_format)
