89ad1e35d35fea04670308050551ba330470f57f
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-03-27 21:33:19 +0900

    Revert lock

--- queue_for_thread/sqs.py
+++ queue_for_thread/sqs.py
@@ -38,11 +38,12 @@ class Sqs():
 
     def receive_message(self, queue_name):
         queue = self.fetch_queue(queue_name)
-        r = queue.receive_messages(MaxNumberOfMessages=1)
-        if len(r) == 0:
-            return None
-        else:
-            message = r[0]
-            body = message.body
-            message.delete()
-            return body
+        with self.lock:
+            r = queue.receive_messages(MaxNumberOfMessages=1)
+            if len(r) == 0:
+                return None
+            else:
+                message = r[0]
+                body = message.body
+                message.delete()
+                return body
